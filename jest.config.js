89c0103ff4ec0995143c5e870d9b62b8d88bcc8a
actionchain <actionchain@chainguard.dev>
2026-05-11 16:20:44 +0000

    Update cschleiden/replace-tokens@v1.4

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  testRunner: 'jest-circus/runner',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!replace-in-file/)'
+  ],
+  verbose: true
+}
