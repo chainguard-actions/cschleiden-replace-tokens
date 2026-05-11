93cb9b5df59a37b70f695b4baa3dbccfc73c7fdd
actionchain <actionchain@chainguard.dev>
2026-05-11 11:50:13 +0000

    Update cschleiden/replace-tokens@v1.1

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import * as core from "@actions/core";
+import { replaceTokens } from "./replace";
+
+function getFiles(): string[] {
+  let files =
+    core.getInput("files", {
+      required: true,
+    }) || "";
+  files = files.replace("\\", "\\\\");
+  if (files.trim().startsWith("[")) {
+    return JSON.parse(files);
+  }
+
+  return [files];
+}
+
+async function run() {
+  try {
+    const tokenPrefix = core.getInput("tokenPrefix") || "#{";
+    const tokenSuffix = core.getInput("tokenSuffix") || "}#";
+    const files = getFiles();
+    const result = await replaceTokens(
+      tokenPrefix,
+      tokenSuffix,
+      Array.isArray(files) ? files : [files]
+    );
+    console.log(`Replaced tokens in files: ${result}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
